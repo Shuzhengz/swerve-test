15d78d5fa5d4d18df8b57573c08218741181e743
Tom Zhang <treez.zhang@gmail.com>
2021-04-21 19:50:35 -0700

    setup TFX

---
+++ src/main/java/com/team1678/swerve/Drive.java
@@ -0,0 +1,89 @@
+package com.team1678.swerve;
+
+import com.team1678.swerve.motorCtrl.TFX;
+
+import com.ctre.phoenix.ErrorCode;
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.util.ArrayList;
+
+public class Drive {
+
+    private static Drive mInstance = new Drive();
+
+    private PeriodicIO mPeriodicIO;
+
+    // Hardware
+    private TalonFX mLeftMaster, mRightMaster, mLeftSlave, mRightSlave;
+
+    private void configureMaster(TalonFX talon, boolean left) {
+        talon.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 5, 100);
+        final ErrorCode sensorPresent = talon.configSelectedFeedbackSensor(TalonFXFeedbackDevice
+                .IntegratedSensor, 0, 100); //primary closed-loop, 100 ms timeout
+        if (sensorPresent != ErrorCode.OK) {
+            DriverStation.reportError("Could not detect " + (left ? "left" : "right") + " encoder: " + sensorPresent, false);
+        }
+        talon.setInverted(left);
+        talon.setSensorPhase(false);
+        talon.enableVoltageCompensation(true);
+        talon.configVoltageCompSaturation(12.0, Constants.kLongCANTimeoutMs);
+        talon.configVelocityMeasurementPeriod(VelocityMeasPeriod.Period_50Ms, Constants.kLongCANTimeoutMs);
+        talon.configVelocityMeasurementWindow(1, Constants.kLongCANTimeoutMs);
+        talon.configClosedloopRamp(Constants.kDriveVoltageRampRate, Constants.kLongCANTimeoutMs);
+        talon.configNeutralDeadband(0.04, 0);
+        talon.configMotionCruiseVelocity(20000, Constants.kLongCANTimeoutMs);
+        talon.configMotionAcceleration(40000, Constants.kLongCANTimeoutMs);
+    }
+
+    private Drive() {
+        mPeriodicIO = new PeriodicIO();
+
+        // Start all Talons in open loop mode.
+        mLeftMaster = TFX.createDefaultTalon(Constants.kLeftDriveMasterId);
+        configureMaster(mLeftMaster, true);
+
+        mLeftSlave = TFX.createPermanentSlaveTalon(Constants.kLeftDriveSlaveId,
+                Constants.kLeftDriveMasterId);
+        mLeftSlave.setInverted(true);
+
+        mRightMaster = TFX.createDefaultTalon(Constants.kRightDriveMasterId);
+        configureMaster(mRightMaster, false);
+
+        mRightSlave = TFX.createPermanentSlaveTalon(Constants.kRightDriveSlaveId,
+                Constants.kRightDriveMasterId);
+        mRightSlave.setInverted(false);
+    }
+
+    public static Drive getInstance() {
+        return mInstance;
+    }
+
+    public static class PeriodicIO {
+        // INPUTS
+        public int left_position_ticks;
+        public int right_position_ticks;
+        public double left_distance;
+        public double right_distance;
+        public double left_current;
+        public double right_current;
+        public int left_velocity_ticks_per_100ms;
+        public int right_velocity_ticks_per_100ms;
+        //public Rotation2d gyro_heading = Rotation2d.identity();
+        //public Pose2d error = Pose2d.identity();
+
+        // OUTPUTS
+        public double left_demand;
+        public double right_demand;
+        public double left_accel;
+        public double right_accel;
+        public double left_feedforward;
+        //public double right_feedforward;
+        //public TimedState<Pose2dWithCurvature> path_setpoint = new TimedState<Pose2dWithCurvature>(Pose2dWithCurvature.identity());
+    }
+}
---
+++ src/main/java/com/team1678/swerve/motorCtrl/Config.java
@@ -0,0 +1,39 @@
+package com.team1678.swerve.motorCtrl;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.VelocityMeasPeriod;
+
+public class Config extends TFX{
+
+    public NeutralMode NEUTRAL_MODE = NeutralMode.Coast;
+    // factory default
+    public double NEUTRAL_DEADBAND = 0.04;
+
+    public boolean ENABLE_CURRENT_LIMIT = false;
+    public boolean ENABLE_SOFT_LIMIT = false;
+    public boolean ENABLE_LIMIT_SWITCH = false;
+    public int FORWARD_SOFT_LIMIT = 0;
+    public int REVERSE_SOFT_LIMIT = 0;
+
+    public boolean INVERTED = false;
+    public boolean SENSOR_PHASE = false;
+
+    public int CONTROL_FRAME_PERIOD_MS = 10;
+    public int MOTION_CONTROL_FRAME_PERIOD_MS = 100;
+    public int GENERAL_STATUS_FRAME_RATE_MS = 10;
+    public int FEEDBACK_STATUS_FRAME_RATE_MS = 100;
+    public int QUAD_ENCODER_STATUS_FRAME_RATE_MS = 1000;
+    public int ANALOG_TEMP_VBAT_STATUS_FRAME_RATE_MS = 1000;
+    public int PULSE_WIDTH_STATUS_FRAME_RATE_MS = 1000;
+
+    public VelocityMeasPeriod VELOCITY_MEASUREMENT_PERIOD = VelocityMeasPeriod.Period_100Ms;
+    public int VELOCITY_MEASUREMENT_ROLLING_AVERAGE_WINDOW = 64;
+
+    public StatorCurrentLimitConfiguration STATOR_CURRENT_LIMIT = new StatorCurrentLimitConfiguration(false, 300, 700, 1);
+    public SupplyCurrentLimitConfiguration SUPPLY_CURRENT_LIMIT = new SupplyCurrentLimitConfiguration(false, 40, 100, 1);
+
+    public double OPEN_LOOP_RAMP_RATE = 0.0;
+    public double CLOSED_LOOP_RAMP_RATE = 0.0;
+}
